bfb6441f2ee769fcb37a8c8b41280cc1f1a44e92
Dorin Pleava <dorin.pleava@Dorins-MacBook-Pro.local>
2022-01-23 21:19:23 +0200

    prepared for azure

--- config/application.rb
+++ config/application.rb
@@ -19,9 +19,6 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
-HOSTNAME = ENV['AZURE_HOST']
-
 module SilverScraper
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
